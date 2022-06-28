02d960f85b2d50822debcaba33d133fb56ffbce8
HoriTakahiro <69907791+HoriTakahiro@users.noreply.github.com>
2022-06-28 09:09:14 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use script';
+
+// 変数 let
+
+let price = 100;
+console.log(price)
+
+// price = price + 100
+price += 100;
+console.log(price);
+
+// price = price * 2 
+price *= 100;
+console.log(price);
+
+// price +=  1
+price ++;
+console.log(price);
+
+// price +=  1
+price --;
+console.log(price);
